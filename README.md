b072aaec5b731937485ec8a4d04b33fdbe74e801
하준혁 <enoxaiming@naver.com>
2016-05-30 10:36:00 +0900

    Add SplashLayout

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # SendU
 SendU
-1
---
+++ SendU/app/src/main/java/handdev/sendu/SplashActivity.java
@@ -0,0 +1,21 @@
+package handdev.sendu;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },3000);
+    }
+}
