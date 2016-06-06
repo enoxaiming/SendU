2915f2566bae933f30e13c2a2d1d83aa05bc2d1b
하준혁 <enoxaiming@naver.com>
2016-06-07 00:46:02 +0900

    Add commingsoon page

--- SendU/app/src/main/java/handdev/sendu/Activity/SplashActivity.java
+++ SendU/app/src/main/java/handdev/sendu/Activity/SplashActivity.java
@@ -1,11 +1,10 @@
 package handdev.sendu.Activity;
 
-import android.content.Intent;
+
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import handdev.sendu.Fragment.FragmentsActivity;
 import handdev.sendu.R;
 
 public class SplashActivity extends AppCompatActivity {
--- SendU/app/src/main/java/handdev/sendu/Fragment/BlankFragment.java
+++ SendU/app/src/main/java/handdev/sendu/Fragment/BlankFragment.java
@@ -2,6 +2,7 @@ package handdev.sendu.Fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
